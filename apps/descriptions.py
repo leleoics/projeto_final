40a16a2afc5b7f811c8a293a7f6b43125d0b9491
leleoics <leonardocartografica@gmail.com>
2022-02-07 17:13:29 -0300

    Alterado texto explicativo sobre a aplicação

--- apps/descriptions.py
+++ apps/descriptions.py
@@ -5,18 +5,17 @@ def inicio(titulo):
     st.title(titulo)
     st.markdown("----")
     st.info("""
-    Esta aplicação esta sendo desenvolvida como projeto final do curso de Engenharia Cartografica e de Agrimensura 
+    Esta é uma aplicação em desenvolvimento como projeto final do curso de Engenharia Cartografica e de Agrimensura 
     da Universidade Federal do Paraná.   
             """)
     st.markdown("""
-    Aqui são apresentados temas de estudos realizados com dados abertos, ferramentas gratuítas e direcionados a região
-    imediata de Curitiba.\n
-    **São apresentado ferramentas como:**\n
-    - Google Earth Engine; .... COMPLEMENTAR\n
-    **A partir disso foi possível construir estudos sobre:**\n
-    - Reservatórios de Água;\n
-    - Crescimento populacional;\n
-    ... COMPLEMENTAR
+    O objetivo desta aplicação é apresentar análises realizadas a partir de imagens orbitais, na região de Curitiba.\n
+    Foram definidas áreas de represas da grande Curitiba como regiões de estudo, para apresentar séries temporais de imagens.\n
+    Além destas regiões, a região de Curitiba também será objeto de estudo, apresentando uma análise sobre o crescimento populacional ao longo dos anos.\n
+        Esta é uma aplicação desenvolvida utilizando as ferramentas:.\n
+    - Google Earth Engine;\n
+    - Streamlit;\n
+    - ....
     """)
     return
 
