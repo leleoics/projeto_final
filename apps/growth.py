3d5adfd259cadba0b4ce1294b82d36e8f7dd3c04
Leonardo Oliveira Melo <leonardocartografica@gmail.com>
2022-03-01 14:02:46 -0300

    Adicionado aba crescimento populacional

---
+++ apps/growth.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import streamlit as st
+import geemap.foliumap as geemap
+import folium
+import ee
+from apps.dam import folium_static
+
+
+def growth(titulo):
+    st.title(titulo)
+    
+    
+    
+    
+    return
+
